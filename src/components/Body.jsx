d4368ecf9b831e56601b8a2d28f4b04f31b53ff8
Bhupesh-aher <aherbhupesh18@gmail.com>
2024-12-22 16:31:30 +0530

    made login secure by adding if token is not present redirect user to login page

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,14 +1,46 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './Navbar'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import Footer from './Footer'
+import { useDispatch, useSelector } from 'react-redux'
+import { BASE_URL } from '../utils/constants'
+import { addUser } from '../utils/userSlice'
+import axios from 'axios'
 
 const Body = () => {
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user)
+  const fetchUser = async() => {
+    try{
+      if(userData) return;
+      const res = await axios(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      })
+      console.log(res);
+      
+      dispatch(addUser(res.data))
+    }
+    catch(err){
+      if(err.status === 401){
+        navigate("/login");
+      }
+      console.log(err);
+      
+    }
+  }
+
+  useEffect(() => {
+   
+      fetchUser();
+   
+  }, [])
+
   return (
     <div>
       <Navbar/>
       <Outlet/>
-     
       <Footer/>
     </div>
   )
